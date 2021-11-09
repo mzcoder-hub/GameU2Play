059e676280338652d66fa7554f4960ef894a4a3e
Mzcoder-hub <masterbama99@gmail.com>
2021-11-09 20:22:16 +0700

    Update component yang ketinggalan

---
+++ frontend/client/components/data/sliderData.js
@@ -0,0 +1,52 @@
+const sliderData = [
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/1.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/2.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/3.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/4.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/1.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/2.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/3.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/4.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/1.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/2.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/3.png',
+  },
+  {
+    title: 'Dota 2 Kiev AniMajor 2021',
+    image: '/images/slides/4.png',
+  },
+]
+
+export default sliderData
